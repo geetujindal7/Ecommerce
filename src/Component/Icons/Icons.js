73b634be7cdfd2a6f8f9cbadb9c1861ae05835f2
Geetika jindal <geetikajindal78@gmail.com>
2022-09-26 23:21:17 +0530

    Ecommerce React

---
+++ src/Component/Icons/Icons.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Icons.css'
+
+function Icons() {
+    return (
+        <>
+            <div className='Icons_container'>
+                <div className='Icons_card'>
+                    <h2>Products</h2>
+                </div>
+                <div className='Icons_card'>
+                    <h2>Cart</h2>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Icons
---
+++ src/Pages/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Icons from '../../Component/Icons/Icons'
+import './Header.css'
+
+function Header() {
+  return (
+    <>
+    <div className='headerMain'>
+      <div className='header_flex'>
+        <h2>SSS</h2>
+        <div className='login_icons'>
+          <h4>Home</h4>
+          <h4>Login</h4>
+          <h4>Support</h4>
+        </div>
+      </div>
+    </div>
+    <Icons />
+    </>
+  )
+}
+
+export default Header
